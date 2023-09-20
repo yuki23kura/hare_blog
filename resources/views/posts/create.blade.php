3d88fda1e23f18740a794e71c4aac9a1adc3a242
yuki23kura <tmdoisii@gmail.com>
2023-09-20 16:43:25 +0900

    リファクタリング

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,18 +2,7 @@
     <div class="container lg:w-1/2 md:w-4/5 w-11/12 mx-auto mt-8 px-8 bg-white shadow-md">
         <h2 class="text-center text-lg font-bold pt-6 tracking-widest">ブログ登録</h2>
 
-        @if ($errors->any())
-            <div class="bg-red-100 border-l-4 border-red-500 text-red-700 p-4 my-2" role="alert">
-                <p>
-                    <b>{{ count($errors) }}件のエラーがあります。</b>
-                </p>
-                <ul>
-                    @foreach($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
+        <x-validation-errors :errors="$errors" />
 
         <form action="{{ route('posts.store') }}" method="POST" enctype="multipart/form-data"
             class="rounded pt-3 pb-8 mb-4">

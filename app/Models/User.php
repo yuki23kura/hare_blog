da1c72b5aa91f4cd6d73452b09c46091da7e5213
yuki23kura <tmdoisii@gmail.com>
2023-09-21 11:34:09 +0900

    コメント登録機能

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,8 +45,11 @@ class User extends Authenticatable
 
     public function posts()
     {
-        return $this ->hasMany(Post::class);
+        return $this->hasMany(Post::class);
     }
-    
 
+    public function comennts()
+    {
+        return $this->belongsTo(Post::class);
+    }
 }
